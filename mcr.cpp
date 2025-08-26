4c6c22abef51488309637e4bb573aab4844ac88d
cs5351author <144524826+cs5351author@users.noreply.github.com>
2025-08-26 16:44:54 +0800

    Update mcr.cpp
    add input checking

--- mcr.cpp
+++ mcr.cpp
@@ -28,6 +28,7 @@ int main(){
 				cout << "Player 2: ";
 			cout << "Which cell to mark? i:[1..3], j:[1..3]: "; 
 			cin >> i >> j;
+		   // for example, add some input checking here for i/j
 			if (turn == false)
 			   game[i][j] = 'X';
 			else 
